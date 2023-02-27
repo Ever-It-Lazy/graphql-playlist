e609d599749b224d63d48f650edbdfe794ec3f9a
Everett Lindsay <everettsjunk@gmail.com>
2023-02-27 15:19:50 -0500

    finished vid #16

--- server/app.js
+++ server/app.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
+const dotenv = require('dotenv');
+
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', false);
 
 const app = express();
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI);
+mongoose.connection.once("open", () => {
+	console.log("connected to database");
+});
 
 app.use("/graphql", graphqlHTTP({
 	schema,
