bf57a887053bd8a3d5f87f28875ad7c5d68ba916
Everett Lindsay <everettsjunk@gmail.com>
2023-02-28 16:53:10 -0500

    addBook() stores in Mongo. However: (a) it doesn't refetch the 1st query (b) server-side validation doesn't prevent blank data (c) error messages don't gracefully return to the client

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,27 +1,52 @@
-import { useQuery } from "@apollo/client";
-import { getAuthorsQuery } from "../queries/queries";
+import { useQuery, useMutation } from "@apollo/client";
+import { useState } from "react";
+import { getAuthorsQuery, addBookMutation } from "../queries/queries";
 
 function AddBook() {
 	const { loading, error, data } = useQuery(getAuthorsQuery);
+	const [addBook, { data: bookData, loading: bookLoading, error: bookError }] = useMutation(addBookMutation, {
+		refetchQueries: [
+			{ query: getAuthorsQuery }, // DocumentNode object parsed with gql
+			'getAuthorsQuery' // Query name
+		]
+	});
+
+	const [name, setName] = useState("");
+	const [genre, setGenre] = useState("");
+	const [authorId, setAuthorId] = useState("");
+
+	const submitForm = (e) => {
+		e.preventDefault();
+		addBook({
+			variables: {
+				name: name,
+				genre: genre,
+				authorId: authorId
+			}
+		});
+	};
 
 	if (loading) return <p>Loading...</p>;
 	if (error) return <p>Error : {error.message}</p>;
 
+	if (bookLoading) return <p>Adding...</p>;
+	if (bookError) return <p>Error : {bookError.message}</p>;
+
 	return (
-		<form id="add-book">
+		<form id="add-book" onSubmit={submitForm}>
 			<div className="field">
 				<label>Book name:</label>
-				<input type="text" />
+				<input type="text" onChange={(e) => setName(e.target.value)} />
 			</div>
 
 			<div className="field">
 				<label>Genre:</label>
-				<input type="text" />
+				<input type="text" onChange={(e) => setGenre(e.target.value)} />
 			</div>
 
 			<div className="field">
 				<label>Author:</label>
-				<select>
+				<select onChange={(e) => setAuthorId(e.target.value)} >
 					<option>Select author</option>
 					{
 						data.authors.map(({ name, id }) => (
