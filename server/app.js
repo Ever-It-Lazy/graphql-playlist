8ada38c0df3c12ece02cadcf886908daca084e5f
Everett Lindsay <everettsjunk@gmail.com>
2023-03-04 00:08:16 -0500

    trying to overcome some PORT settings

--- server/app.js
+++ server/app.js
@@ -19,6 +19,9 @@ app.use("/graphql", graphqlHTTP({
 	graphiql: true
 }));
 
-app.listen(4000, () => {
-	console.log("now listening for requests on port 4000");
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+	console.log(`now listening for requests on port ${PORT}`);
 });
